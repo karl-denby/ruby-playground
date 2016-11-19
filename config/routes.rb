92311c94fe6183f4e564f3333466551849ccbc9f
karldenby <karl.denby@gmail.com>
2016-11-19 21:52:41 +0000

    user can create account and log in

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,13 @@ Rails.application.routes.draw do
   resources :about
   resources :users
 
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+  
   get '/login',to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   
-
-  
-
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
